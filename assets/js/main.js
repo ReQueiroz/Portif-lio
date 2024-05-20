c71e6bee641555d2241c149d186855505d69be5e
ReQueiroz <159490585+ReQueiroz@users.noreply.github.com>
2024-05-20 15:17:13 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,8 +137,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 30,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
